d53174a4df4a8f5ef2f331df7cac782f835feccb
brostacia <58284794+brostacia@users.noreply.github.com>
2020-04-11 16:48:20 +0700

    Three variables

--- Nastya_Project/src/HelloWorld.java
+++ Nastya_Project/src/HelloWorld.java
@@ -4,9 +4,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 	    int n1 = 5;
 	    int n2 = 10;
+	    int n3 = 12;
 	    int res;
-	    if (n2 > n1)
-		res = n2; else
+	    if (n2 > n1 && n3 > n2)
+		res = n2 + n3; else
 		res = n1;
 	    System.out.println(res);
 	    	}
