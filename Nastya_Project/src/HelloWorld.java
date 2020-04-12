ce57a1867109095e4bfb56d34df7425fd8cdeb0c
brostacia <58284794+brostacia@users.noreply.github.com>
2020-04-12 13:05:15 +0700

    Finally done

--- Nastya_Project/src/HelloWorld.java
+++ Nastya_Project/src/HelloWorld.java
@@ -1,15 +1,13 @@
+//вывести элементы массива в обратном порядке
 
 public class HelloWorld {
-
 	public static void main(String[] args) {
-	    int a[] = new int[3];
-	    a[0] = 5;
-	    a[1] = 10;
-	    a[2] = 12;
-	    int res = 0;
-	    for(int i = 0;i < a.length;i++)
-		if (a[i] > res)
-		    res = a[i];
-	    System .out.println(res);
-	    	    	}
+		int a[] = new int[3]; 
+		a[0] = 5;
+		a[1] = 10;
+		a[2] = 12;
+		for(int i = a.length - 1; i >= 0; i--) {
+			System.out.println(a[i]);
+		}
+	}
 }
