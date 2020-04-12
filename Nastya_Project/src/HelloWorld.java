6e7053cfbfb6861147a54bd49f5b4a69ecdbfa81
Michael Pozhidaev <msp@luwrain.org>
2020-04-12 10:14:41 +0700

    New morning exercise

--- Nastya_Project/src/HelloWorld.java
+++ Nastya_Project/src/HelloWorld.java
@@ -2,18 +2,14 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-	    int n1 = 5;
-	    int n2 = 10;
-	    int n3 = 12;
-	    int res;
-	    
-	    if (n2 > n1 && n2 > n3) {
-		res = n2; 
-		} else if (n3 > n1 && n3 > n2) {
-	    res = n3;
-		} else {
-		res = n1;
-		}
-		System.out.println(res);
-	    	}
+	    int a[] = new int[3];
+	    a[0] = 5;
+	    a[1] = 10;
+	    a[2] = 12;
+	    int res = 0;
+	    for(int i = 0;i < a.length;i++)
+		if (a[i] > res)
+		    res = a[i];
+	    System .out.println(res);
+	    	    	}
 }
