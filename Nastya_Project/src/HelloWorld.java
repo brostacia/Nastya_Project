010caf8b85155fd38a2c9535e46e275193f7e37c
Michael Pozhidaev <msp@luwrain.org>
2020-04-14 12:45:07 +0700

    New fragment to recognize

--- Nastya_Project/src/HelloWorld.java
+++ Nastya_Project/src/HelloWorld.java
@@ -4,13 +4,11 @@ import java.util.Arrays;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		int[] a = {5, 10, 12, 30, 45, 67, 89, 100};
-		int temp;
-		for(int i = 0;i < a.length / 2;i++) {
-			temp = a[a.length-i-1];
-			a[a.length-i-1] = a[i];
-			a[i] = temp;
-	    }
-		System.out.println(Arrays.toString(a));
+	    int count = 0;
+	    String str = "Какой прекрасный день!";
+	    for(int i = 0;i < str.length();i++)
+		if (str.charAt(i) == ' ')
+		    count++;
+	    		System.out.println(count);
 	}
 }	
