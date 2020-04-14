b40d2b6c4686ec29fb0326447b562b12117b8cfb
brostacia <58284794+brostacia@users.noreply.github.com>
2020-04-14 16:05:28 +0700

    more than once

--- Nastya_Project/src/HelloWorld.java
+++ Nastya_Project/src/HelloWorld.java
@@ -1,14 +1,17 @@
-//вывести элементы массива в обратном порядке
-
-import java.util.Arrays;
+//Вывести на экран все символы, которые в строке встречаются больше одного раза.
 
 public class HelloWorld {
 	public static void main(String[] args) {
-	    int count = 0;
-	    String str = "Какой прекрасный день!";
-	    for(int i = 0;i < str.length();i++)
-		if (str.charAt(i) == ' ')
-		    count++;
-	    		System.out.println(count);
-	}
-}	
+		String str = "Какой прекрасный день!";
+		char[] chArray = str.toCharArray();
+		      
+        for(int i=0;i<chArray.length;i++){
+            char ch1 = chArray[i];
+            for (int j=i+1; j<chArray.length; j++){
+                if(ch1==chArray[j]){
+                    System.out.println(ch1);
+                }
+            }
+	   }
+   }
+}
