16e767488569bbd9fa05289422912046f3a72d7f
Michael Pozhidaev <msp@luwrain.org>
2020-04-14 12:19:50 +0700

    The initial line for the foor() loop, trying to reverce an array

--- Nastya_Project/src/HelloWorld.java
+++ Nastya_Project/src/HelloWorld.java
@@ -4,13 +4,11 @@ import java.util.Arrays;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		int[] a = {5, 10, 12,}; 	
-		int temp = a[0];
-        a[0] = a[2];
-        a[2] = temp;
-		{
-		String intArrayString = Arrays.toString(a);
-		System.out.println(intArrayString);
-		}
+		int[] a = {5, 10, 12,};
+		for(int i = 0;i < a.length / 2;i++)
+	    {
+		
+	    }
+		System.out.println(Arrays.toString(a));
+	}
 	}	
-}
