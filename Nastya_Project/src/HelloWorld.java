2755959ec6cd50911bf6915620ac54bbdf050942
brostacia <58284794+brostacia@users.noreply.github.com>
2020-04-14 12:38:44 +0700

    Done

--- Nastya_Project/src/HelloWorld.java
+++ Nastya_Project/src/HelloWorld.java
@@ -4,11 +4,13 @@ import java.util.Arrays;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		int[] a = {5, 10, 12,};
-		for(int i = 0;i < a.length / 2;i++)
-	    {
-		
+		int[] a = {5, 10, 12, 30, 45, 67, 89, 100};
+		int temp;
+		for(int i = 0;i < a.length / 2;i++) {
+			temp = a[a.length-i-1];
+			a[a.length-i-1] = a[i];
+			a[i] = temp;
 	    }
 		System.out.println(Arrays.toString(a));
 	}
-	}	
+}	
