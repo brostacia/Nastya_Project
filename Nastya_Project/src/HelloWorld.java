eab97ce589f4f875acbc02406603f2bc6d6a49e7
brostacia <58284794+brostacia@users.noreply.github.com>
2020-04-15 14:28:17 +0700

    Repeated letters

--- Nastya_Project/src/HelloWorld.java
+++ Nastya_Project/src/HelloWorld.java
@@ -2,16 +2,17 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		String str = "Какой прекрасный день, и светит солнце!";
-		char[] chArray = str.toCharArray();
-		      
-        for(int i=0;i<chArray.length;i++){
-            char ch1 = chArray[i];
-            for (int j=i+1; j<chArray.length; j++){
-                if(ch1==chArray[j]){
-                    System.out.println(ch1);
-                }
+		String str = ("Какой прекрасный день, и светит солнце!").toLowerCase();   
+		 
+        for(int i=0;i<str.length();i++){
+            char ch1 = str.charAt(i);
+            for (int j=i+1; j<str.length(); j++){
+            	char ch2 = str.charAt(j);
+            	if(ch1==ch2){
+            		System.out.println(ch1);
+			    	break;
             }
 	   }
    }
+}
 }
