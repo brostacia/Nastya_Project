d0ffbe11dd87e485107fc7a4c68155dad8ee73d2
brostacia <58284794+brostacia@users.noreply.github.com>
2020-04-18 13:03:41 +0700

    Last commit

--- Nastya_Project/src/HelloWorld.java
+++ Nastya_Project/src/HelloWorld.java
@@ -2,17 +2,23 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		String str = ("Какой прекрасный день, и светит солнце!").toLowerCase();   
-		 
-        for(int i=0;i<str.length();i++){
-            char ch1 = str.charAt(i);
-            for (int j=i+1; j<str.length(); j++){
-            	char ch2 = str.charAt(j);
-            	if(ch1==ch2){
-            		System.out.println(ch1);
-			    	break;
-            }
-	   }
-   }
-}
-}
+		String str = ("Какой прекрасный день, и светит солнце!").toLowerCase();
+		int countLeft = 0;
+		int countRight = 0;
+		int countnew = 0;
+		
+		for(int i=0;i<str.length();i++){
+        	for (int j = 0;j < str.length();j++) {
+        		if (str.charAt(i) != str.charAt(j)) 
+        			continue; 
+        		if (j < i) 
+        			countLeft++; 
+        		if (j > i)
+        			countRight++; 
+        		if (countLeft == 0 & countRight > 0)
+        			System.out.println(str.charAt(j));
+		}
+	}
+}	
+}		
+	
