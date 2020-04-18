3934545c46c3cf80a8457b62b93d5890a424656d
Michael Pozhidaev <msp@luwrain.org>
2020-04-18 13:09:30 +0700

    Fixes to the search of chars used twice and more

--- Nastya_Project/src/HelloWorld.java
+++ Nastya_Project/src/HelloWorld.java
@@ -1,24 +1,21 @@
 //Вывести на экран все символы, которые в строке встречаются больше одного раза.
 
 public class HelloWorld {
-	public static void main(String[] args) {
-		String str = ("Какой прекрасный день, и светит солнце!").toLowerCase();
-		int countLeft = 0;
-		int countRight = 0;
-		int countnew = 0;
-		
-		for(int i=0;i<str.length();i++){
-        	for (int j = 0;j < str.length();j++) {
-        		if (str.charAt(i) != str.charAt(j)) 
-        			continue; 
-        		if (j < i) 
-        			countLeft++; 
-        		if (j > i)
-        			countRight++; 
-        		if (countLeft == 0 & countRight > 0)
-        			System.out.println(str.charAt(j));
-		}
+    public static void main(String[] args) {
+	String str = ("Какой прекрасный день, и светит солнце!").toLowerCase();
+	for(int i=0;i<str.length();i++){
+	    int countLeft = 0;
+	    int countRight = 0;
+	    for (int j = 0;j < str.length();j++) {
+		if (str.charAt(i) != str.charAt(j)) 
+		    continue; 
+		if (j < i) 
+		    countLeft++; 
+		if (j > i)
+		    countRight++; 
+	    }
+	    if (countLeft == 0 & countRight > 0)
+		System.out.println(str.charAt(i));
 	}
-}	
+    }	
 }		
-	
