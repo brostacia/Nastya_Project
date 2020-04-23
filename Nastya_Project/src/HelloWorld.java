d23af2c98baaebb48355253d3e0fe15499b08d9f
brostacia <58284794+brostacia@users.noreply.github.com>
2020-04-23 10:51:34 +0700

    letters

--- Nastya_Project/src/HelloWorld.java
+++ Nastya_Project/src/HelloWorld.java
@@ -3,10 +3,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
 	String str = ("Какой прекрасный день, и светит солнце!").toLowerCase();
+	
 	int[] letters = new int['я' + 1];
 	for(int i = 0;i < letters.length;i++)
 	    letters[i] = 0;
 	for(int i = 0;i < str.length();i++)
-	    letters[str.charAt(i)]++;
-    }  
+	    letters[str.charAt(i)]++;	
+	System.out.println(letters); 
  }
+}
