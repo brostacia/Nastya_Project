18d4b9f6b05cd7acf623684bac5dad34476a8ccf
brostacia <58284794+brostacia@users.noreply.github.com>
2020-04-23 09:32:42 +0700

    help

--- Nastya_Project/src/HelloWorld.java
+++ Nastya_Project/src/HelloWorld.java
@@ -3,19 +3,17 @@
 public class HelloWorld {
     public static void main(String[] args) {
 	String str = ("Какой прекрасный день, и светит солнце!").toLowerCase();
-	for(int i=0;i<str.length();i++){
-	    int countLeft = 0;
-	    int countRight = 0;
-	    for (int j = 0;j < str.length();j++) {
-		if (str.charAt(i) != str.charAt(j)) 
-		    continue; 
-		if (j < i) 
-		    countLeft++; 
-		if (j > i)
-		    countRight++; 
-	    }
-	    if (countLeft == 0 & countRight > 0)
-		System.out.println(str.charAt(i));
-	}
-    }	
-}		
+	char[] chArray = str.toCharArray();
+	
+	//массив такой длины, сколько букв в русском языке. По умолчанию заполняется нулями.
+	int Zero[];
+	Zero = new int[33];
+		
+    //по строке
+    for(int i=0;i<chArray.length;i++){
+    	int index=0;
+    	Zero[index] += 1;
+    	}    	
+    System.out.println(Zero);
+    }  
+ }
