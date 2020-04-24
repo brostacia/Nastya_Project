f4c4c2e41eb9768d251c6c5271eedbd9d326bfe8
Michael Pozhidaev <msp@luwrain.org>
2020-04-24 09:22:12 +0700

    Adding a class for counting words occurrences

--- Nastya_Project/src/HelloWorld.java
+++ Nastya_Project/src/HelloWorld.java
@@ -1,9 +1,18 @@
 //поиск повторяющихся слов в тексте
 
+import java.util.*;
+
+class Word
+{
+    String word = "";
+    int count = 0;
+}
+
 public class HelloWorld {
     public static void main(String[] args) {
 	String str = ("Какой сегодня день, чудесный день!").toLowerCase().replaceAll("[^a-zA-Zа-яА-Я]", " ");
 	String[] words = str.split("\\s+");
+	List<Word> occurrences = new LinkedList();
 	for(String subStr:words) {
         System.out.println(subStr); 
 	}
