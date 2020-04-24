2abe542f2b333715f059ac685a8b63301047b859
brostacia <58284794+brostacia@users.noreply.github.com>
2020-04-24 08:34:19 +0700

    New task

--- Nastya_Project/src/HelloWorld.java
+++ Nastya_Project/src/HelloWorld.java
@@ -1,14 +1,10 @@
+//поиск повторяющихся слов в тексте
 public class HelloWorld {
     public static void main(String[] args) {
-	String str = ("Какой прекрасный день, и светит солнце!").toLowerCase();
-	
-	int[] letters = new int['я' + 1];
-	for(int i = 0;i < letters.length;i++)
-	    letters[i] = 0;
-	for(int i = 0;i < str.length();i++)
-	    letters[str.charAt(i)]++;	
-	for(int i = 0;i < str.length();i++)
-		if (letters[str.charAt(i)] > 1)		
-	System.out.println(str.charAt(i));
- }
+	String str = ("Какой сегодня день, чудесный день!").toLowerCase();
+	String[] words = str.split("\\s");
+	for(String subStr:words) {
+        System.out.println(subStr); 
+	}
+  }
 }
