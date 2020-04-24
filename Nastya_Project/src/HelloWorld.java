29357085ea40144b2d735f52cf0dc571c99e7977
brostacia <58284794+brostacia@users.noreply.github.com>
2020-04-24 09:01:04 +0700

    no punctuation

--- Nastya_Project/src/HelloWorld.java
+++ Nastya_Project/src/HelloWorld.java
@@ -1,10 +1,12 @@
 //поиск повторяющихся слов в тексте
+
 public class HelloWorld {
     public static void main(String[] args) {
-	String str = ("Какой сегодня день, чудесный день!").toLowerCase();
-	String[] words = str.split("\\s");
+	String str = ("Какой сегодня день, чудесный день!").toLowerCase().replaceAll("[^a-zA-Zа-яА-Я]", " ");
+	String[] words = str.split("\\s+");
 	for(String subStr:words) {
         System.out.println(subStr); 
 	}
   }
 }
+ 
