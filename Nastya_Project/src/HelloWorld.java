c234e1a8e544ff1a6fcd061492e423afe62d14d6
brostacia <58284794+brostacia@users.noreply.github.com>
2020-04-27 08:54:24 +0700

    how I think it will look like

--- Nastya_Project/src/HelloWorld.java
+++ Nastya_Project/src/HelloWorld.java
@@ -12,12 +12,14 @@ public class HelloWorld {
     public static void main(String[] args) {
 	String str = ("Какой сегодня день, чудесный день!").toLowerCase().replaceAll("[^a-zA-Zа-яА-Я]", " ");
 	String[] words = str.split("\\s+");
-	List<Word> occurrences = new LinkedList();
-	for(String subStr:words) {
-        occurrences.add(subStr);
-        count++;
-       if (count > 1) 
-    	   System.out.println(occurrences);
+	
+	List<Word> occurrences = new LinkedList();;
+	for(int i = 0; i < words.length; i++) {
+		occurrences.add(i,words[i]);
+		count++;
+	if (count > 1) 
+		System.out.println(occurrences);
 	}
    }
 }
+
