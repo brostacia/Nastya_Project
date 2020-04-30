d82b7a83d99c0404ab1e5fc4793740518d787f73
Michael Pozhidaev <msp@luwrain.org>
2020-04-30 13:06:09 +0700

    Counting the words in a sentence

--- Nastya_Project/src/HelloWorld.java
+++ Nastya_Project/src/HelloWorld.java
@@ -10,21 +10,24 @@ public class HelloWorld {
     public static void main(String[] args) {
 	String str = ("Какой сегодня день, чудесный день!").toLowerCase().replaceAll("[^a-zA-Zа-яА-Я]", " ");
 	String[] words = str.split("\\s+");
-	
 	List<Word> occurrences = new LinkedList<Word>();
 	for(String i: words){
-		Word w = new Word();
-		w.word = i;
-		w.count = 1;
+	    boolean found = false;
+	    for(Word w: occurrences)
+		if (w.word.equals(i))
+		{
+		    w.count++;
+		    found = true;
+		    break;
+		}
+	    if (found)
+		continue;
+	    final Word w = new Word();
+	    w.word = i;
+	    w.count = 1;
+	    occurrences.add(w);
 	}
-	{
 	for(Word w: occurrences)
-		if (!occurrences.contains(w))
-			occurrences.add(w);
-		else
-			occurrences.add(w.count + 1,w);
-	}		
-	for(Word w: occurrences)
-		System.out.println(w.word + ": " + w.count);
-	}
+	    System.out.println(w.word + ": " + w.count);
+    }
 }
