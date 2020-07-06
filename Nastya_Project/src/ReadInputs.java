67cffda3f305fe901ef73d748afabed9e6fd5518
brostacia <58284794+brostacia@users.noreply.github.com>
2020-07-06 08:41:33 +0700

    variable and count

--- Nastya_Project/src/ReadInputs.java
+++ Nastya_Project/src/ReadInputs.java
@@ -4,21 +4,28 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class ReadInputs {
 	
 	public static void main(String[] args) {
 		
-		File file = new File("Nastya_Project/src/file.java");
+		File file = new File("Nastya_Project/src/file.txt");
 		try (FileReader fileReader = new FileReader(file)) {
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
 			String line = null;
 			while ((line = bufferedReader.readLine()) != null) {
-				Tokenizer.tokenize(line);
-				System.out.println((String) line);
+				Token tokens [] = Tokenizer.tokenize(line);
+				int count=0;
+				for (Token t : tokens) {
+					count ++;
+				}
+				System.out.println(count);
+				System.out.println(Arrays.toString(tokens));
 			}
 		} catch (Exception e) {
 		}
+		
 	}
 }
 
