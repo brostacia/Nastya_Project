585eb4cb1bfa45f80a32a7e4d07bcf30684f10e9
brostacia <58284794+brostacia@users.noreply.github.com>
2020-07-07 09:53:28 +0700

    t.text

--- Nastya_Project/src/ReadInputs.java
+++ Nastya_Project/src/ReadInputs.java
@@ -20,6 +20,31 @@ public class ReadInputs {
 				Token tokens [] = Tokenizer.tokenize(line);
 				for (Token t : tokens) {
 					for(String h : Homonyms.PARADIGMATIC) {
+						if (t.text.contains(h)) 
+							homographs.add(t.text);
+					}			
+				}
+				System.out.println(Arrays.toString(tokens));
+				System.out.println("Количество токенов: " + tokens.length);	
+			}
+			System.out.println("Парадигматические омографы: " + homographs);
+		} catch (Exception e) {
+		}
+	}
+}
+
+
+/*
+
+	ArrayList<String> homographs = new ArrayList<String>();
+		File file = new File("Nastya_Project/src/file.txt");
+		try (FileReader fileReader = new FileReader(file)) {
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			String line = null;
+			while ((line = bufferedReader.readLine()) != null) {
+				Token tokens [] = Tokenizer.tokenize(line);
+				for (Token t : tokens) {
+					for(String h : Homonyms.PARADIGMATIC) {
 						if (t.toString().contains(h)) 
 							homographs.add(t.toString());
 					}			
@@ -33,3 +58,4 @@ public class ReadInputs {
 		
 	}
 }
+*/
