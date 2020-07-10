c567cff94d768329d0316bf769eee50b574f7d3e
brostacia <58284794+brostacia@users.noreply.github.com>
2020-07-10 10:21:02 +0700

    Final

--- Nastya_Project/src/ReadInputs.java
+++ Nastya_Project/src/ReadInputs.java
@@ -11,7 +11,7 @@ public class ReadInputs {
 	
 	public static void main(String[] args) {
 		
-		//ArrayList<String> homographs = new ArrayList<String>();
+		ArrayList<String> homographs = new ArrayList<String>();
 		File file = new File("Nastya_Project/src/file.txt");
 		try (FileReader fileReader = new FileReader(file)) {
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -21,17 +21,17 @@ public class ReadInputs {
 				for (int i=0; i<tokens.length; i++) {
 					for(int j=0; j<Homonyms.PARADIGMATIC.length; j++) {
 						if (tokens[i].text.equals(Homonyms.PARADIGMATIC[j])) {
-							//homographs.add(tokens[i].text); // омограф
+						homographs.add(tokens[i].text); // омограф
 							int count = 1;
-							while (tokens[i-count].type != Token.Type.CYRIL && i>=0) {
-										count++;							
+							while (count <= i && tokens[i-count].type != Token.Type.CYRIL) {
+										count++;	
 									}
 							System.out.println(tokens[i-count]);	
 							}
 						}
 					}
 			}
-				//System.out.println(homographs);
+				System.out.println(homographs);
 			//	System.out.println(Arrays.toString(tokens));
 		} catch (Exception e) {
 		}
@@ -39,3 +39,6 @@ public class ReadInputs {
 	}
 }
 
+// Тут условие должно быть просто математическим сравнением.
+// count унести за равно
+// ноль ещё пока является допустимым значением, а вот меньше нуля нельзя.
